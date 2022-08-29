daa7cf7a4be8e0b99008736b8795897d02512d4a
Dennis Zorn <56258612+PulsarNeutronStar@users.noreply.github.com>
2022-08-29 23:16:17 +0200

    acre uncon 1

--- core/def/param.hpp
+++ core/def/param.hpp
@@ -453,7 +453,7 @@ class Params {
         title = "CVO - ACRE - Unconcious Langauge";
         values[]={0,1};
         texts[]={"Off","On"};
-        default = 0;
+        default = 1;
     };
    class cvo_p_arsenal { // CVO - Custom Arsenal
         title = "CVO - ARSENAL - Custom Ace Arsenal";
