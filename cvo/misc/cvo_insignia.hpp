efcd704b147df80b5b4930873aa137a54829ec87
Dennis Zorn <56258612+PulsarNeutronStar@users.noreply.github.com>
2022-10-10 23:59:07 +0200

    added cvo insignia

---
+++ cvo/misc/cvo_insignia.hpp
@@ -0,0 +1,96 @@
+class cvo11
+{	
+	displayName = "24th CVO 11 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO11Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo12
+{
+	displayName = "24th CVO 12 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO12Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo13
+{
+	displayName = "24th CVO 13 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO13Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo14
+{	
+	displayName = "24th CVO 14 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO14Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo15
+{
+	displayName = "24th CVO 15 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO15Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo16
+{
+	displayName = "24th CVO 16 Green";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO16Insignia.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo11black
+{
+	displayName = "24th CVO 11 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO11Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo12black
+{
+	displayName = "24th CVO 12 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO12Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo13black
+{
+	displayName = "24th CVO 13 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO13Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo14black
+{
+	displayName = "24th CVO 14 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO14Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo15black
+{
+	displayName = "24th CVO 15 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO15Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
+class cvo16black
+{
+	displayName = "24th CVO 16 Black";										// Name displayed in Arsenal
+	author = "24th Chorni Voron";											// Author displayed in Arsenal
+	texture = "cvo\img\CVO16Insigniablack.paa";									// Image path
+	material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";	// .rvmat path
+	textureVehicle = "";													// Does nothing, reserved for future use
+};
